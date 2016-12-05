c6618838830e39f44ec46c04f1f284064201b6ed
Mirko Cukich <mirkocukich@gmail.com>
2016-12-05 14:22:22 -0600

    Initial commit

---
+++ src/com/digitalmirko/Main.java
@@ -0,0 +1,19 @@
+package com.digitalmirko;
+
+// Binary Search Algorithm in Java
+// This algorithm goes and compares the Key value requested with the Key Value
+// of the middle element of the array. Then searches for its position as it breaks
+// down the array more to find its location postion.
+
+// Big O Notation:
+// Best case: O(1), Worst Case: O(log n)
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
